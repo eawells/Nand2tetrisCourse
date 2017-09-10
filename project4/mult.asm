ba7921fa1f07a3a9cd3b130837a58c2ae698b0b4
eawells <elizabeth.a.wells1@gmail.com>
2017-09-10 17:23:54 -0500

    Added functionality to the multiplier

--- project4/mult.asm
+++ project4/mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+	@i
+	M=1
+	@2
+	M=0
+(LOOP)
+	@i
+	D=M
+	@1
+	D=D-M //D=i-RAM[1]
+	@END
+	D;JGT 
+	@0
+	D=M
+	@2
+	M=D+M
+	@i
+	M=M+1
+	@LOOP
+	0; JMP //Goto LOOP
+(END)
+	@END
+	0; JMP
