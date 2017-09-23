63df25761f994d4ff0a0c1ad882f28e681d1493b
eawells <elizabeth.a.wells1@gmail.com>
2017-09-23 16:24:46 -0500

    Second test of parser

--- project6/Assembler/src/main/java/assembler/Parser.java
+++ project6/Assembler/src/main/java/assembler/Parser.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Parser {
 	private List<List<String>> listOfParsedStrings;
-	
+
 	public Parser(InputStream file){
 		listOfParsedStrings = new ArrayList<List<String>>();
 	}
@@ -13,7 +13,9 @@ public class Parser {
 	public void parseString(String string) {
 		List<String> parsedString = new ArrayList<String>();
 		listOfParsedStrings.add(parsedString);
-		parsedString.add("@");
+		for(int i= 0; i<string.length(); i++){
+			parsedString.add(string.substring(i,i+1));
+		}
 	}
 
 	public List<List<String>> getList() {
--- project6/Assembler/src/test/java/tests/ParserTests.java
+++ project6/Assembler/src/test/java/tests/ParserTests.java
@@ -4,18 +4,36 @@ import static org.junit.Assert.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 
 import org.junit.Test;
 
 import assembler.Parser;
 
 public class ParserTests {
-	
+
+	private Parser p;
+
+	@Before
+	public void setUp() throws FileNotFoundException{
+		p = new Parser(new FileInputStream("c:/Users/littl/Dropbox/Summer 17/nand2tetris/nand2tetris/projects/06/add/At2.asm"));
+	}
+
 	@Test
-	public void whenTheParserReadsInAt2ThenAtIsFirstElementInList() throws FileNotFoundException{
-		Parser p = new Parser(new FileInputStream("c:/Users/littl/Dropbox/Summer 17/nand2tetris/nand2tetris/projects/06/add/At2.asm"));
-		p.parseString("@2");
+	public void whenTheParserReadsInAtThenAtIsFirstElementInList(){
+		p.parseString("@");
 		assertEquals("@",p.getList().get(0).get(0));
+	}
+
+	@Test public void whenTheParserReadsInAt2Then2IsSecondElementInList(){
+		p.parseString("@2");
+		List<String> parsedString = new ArrayList<String>();
+		parsedString.add("@");
+		parsedString.add("2");
+		assertEquals(parsedString,p.getList().get(0));
 	}
 
 }
